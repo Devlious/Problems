6639db99a691d45b63c8e50f0b8870cd32082879
Devlious <humberto.dev96@gmail.com>
2018-04-27 00:00:51 -0500

    First C program + imgui setup

---
+++ c/first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+
+	int r, c;
+
+	printf("Introduce Row & Column: \n");
+	scanf("%d %d", &r, &c);
+
+	for(int i = 1; i <= r; i++){
+		for(int j = 1; j <= c; j++)
+			printf("%3d ", i * j);
+		printf("\n");
+	}
+
+	return 0;
+}
