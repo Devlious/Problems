5b0ba5817b44cc15c23655c7ea9950d3f12ffc57
Devlious <humberto.dev96@gmail.com>
2018-05-16 22:51:13 -0500

    Hackerrank rectangleArea added

---
+++ c++/hackerrank/rectangleArea.cpp
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  rectangleArea.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/16/2018 22:49:18
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Humberto Esparza (Devlious), humberto.dev96@gmail.com
+ *        Company:  Blinkers
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+
+	int W, H;
+
+	scanf("%d %d", &W, &H);
+
+	printf("%d %d\n%d", W, H, W*H);
+
+	return 0;
+}
